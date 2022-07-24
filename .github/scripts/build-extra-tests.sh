87ce90e97797aafab25bd66cc2551ce517f22227
abejgonzalez <abe.j.gonza@gmail.com>
2022-07-24 13:09:29 -0700

    test

--- .github/scripts/build-extra-tests.sh
+++ .github/scripts/build-extra-tests.sh
@@ -13,6 +13,8 @@ export PATH="$RISCV/bin:$PATH"
 
 ls -alh $RISCV
 ls -alh $RISCV/riscv64-unknown-elf/lib
+conda env list
+find $CONDA_PREFIX -name "libmpc.*"
 
 echo 'int main(void) { return 0; }' | riscv64-unknown-elf-gcc -xc -specs=htif_nano.specs -o /dev/null -
 
