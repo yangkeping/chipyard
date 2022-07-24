680b9cd33a7b82a669c8ee7a4cfec7204c3e78e1
abejgonzalez <abe.j.gonza@gmail.com>
2022-07-24 12:52:09 -0700

    test

--- .github/scripts/build-extra-tests.sh
+++ .github/scripts/build-extra-tests.sh
@@ -14,5 +14,7 @@ export PATH="$RISCV/bin:$PATH"
 ls -alh $RISCV
 ls -alh $RISCV/riscv64-unknown-elf/lib
 
+echo 'int main(void) { return 0; }' | riscv64-unknown-elf-gcc -xc -specs=htif_nano.specs -o /dev/null -
+
 make -C $LOCAL_CHIPYARD_DIR/tests clean
 make -C $LOCAL_CHIPYARD_DIR/tests
